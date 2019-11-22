f4f6b0acd07edf1891b8125e71b90cf6c85ad730
hspark <phaesoo@gmail.com>
2019-11-22 07:26:07 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='regista',
+    version='1.0.0',
+    author='haesoo',
+    packages=['regista'],
+    include_package_data=True,
+    install_requires=[
+    ],
+)
